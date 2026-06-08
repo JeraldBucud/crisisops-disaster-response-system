611e443984c38c034e7eeb4cc8c8e315a25e771f
leocardosoe <l.cardosorodriguez@cqumail.com>
2026-06-08 15:28:05 +1000

    commit

--- src/drsinitial/controller/MainDashboardController.java
+++ src/drsinitial/controller/MainDashboardController.java
@@ -45,7 +45,7 @@ import drsinitial.repository.ApplicationRepository;
  * @studentId 12301099
  * @course COIT20258 Software Engineering
  */
-public class MainDashboardController {
+public class MainDashboardController { 
 
     private final IncidentService incidentService = new IncidentService();
 
