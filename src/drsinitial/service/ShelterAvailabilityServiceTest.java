9edbaef0fc0b7382f6073a1797d8fa07614389ad
leocardosoe <leocardosoe@gmail.com>
2026-06-08 14:08:41 +1000

    commit

---
+++ src/drsinitial/service/ShelterAvailabilityServiceTest.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package drsinitial.service;
+
+import drsinitial.model.EvacuationShelter;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Cardoso Pepe
+ */
+
+public class ShelterAvailabilityServiceTest {
+
+    private final ShelterAvailabilityService service = new ShelterAvailabilityService();
+
+    @Test
+    public void testCalculateAvailableSpaces() {
+        EvacuationShelter shelter = new EvacuationShelter(
+                "SH003", "Community Centre", "Fortitude Valley",
+                120, 45, "Available", "2026-06-08"
+        );
+
+        assertEquals(75, service.calculateAvailableSpaces(shelter));
+    }
+
+    @Test
+    public void testNearCapacityStatus() {
+        EvacuationShelter shelter = new EvacuationShelter(
+                "SH004", "Sports Hall", "New Farm",
+                100, 95, "Available", "2026-06-08"
+        );
+
+        assertEquals("Near Capacity", service.determineShelterStatus(shelter));
+    }
+
+    @Test
+    public void testFullStatus() {
+        EvacuationShelter shelter = new EvacuationShelter(
+                "SH005", "Library Shelter", "Toowong",
+                100, 100, "Available", "2026-06-08"
+        );
+
+        assertEquals("Full", service.determineShelterStatus(shelter));
+    }
+}
---
+++ src/drsinitial/service/ShelterValidationServiceTest.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package drsinitial.service;
+
+import drsinitial.model.EvacuationShelter;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Cardoso Pepe
+ */
+
+
+public class ShelterValidationServiceTest {
+
+    private final ShelterValidationService service = new ShelterValidationService();
+
+    @Test
+    public void testValidShelter() {
+        EvacuationShelter shelter = new EvacuationShelter(
+                "SH001", "City Hall Shelter", "Brisbane CBD",
+                100, 60, "Available", "2026-06-08"
+        );
+
+        assertTrue(service.isValidShelter(shelter));
+    }
+
+    @Test
+    public void testInvalidShelterCapacity() {
+        EvacuationShelter shelter = new EvacuationShelter(
+                "SH002", "School Shelter", "South Bank",
+                50, 70, "Available", "2026-06-08"
+        );
+
+        assertFalse(service.isValidShelter(shelter));
+    }
+}
